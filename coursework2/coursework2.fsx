aa107a054694dc21966c6ff6af887984634bf19a
Tavo Annus <tavo.annus@gmail.com>
2021-09-30 07:35:35 +0300

    .

--- coursework2/coursework2.fsx
+++ coursework2/coursework2.fsx
@@ -73,10 +73,11 @@ let bibliographyData: BibliographyItem list = [
 let rec compareLists xs ys =
   match (xs, ys) with
     | ([], []) -> 0
+    | ([],  _) -> 1
+    | (_,  []) -> -1
     | (x :: xs, y :: ys) -> match System.String.Compare(x, y) with  // <- Can we write it in "functional" way?
                             | 0 -> compareLists xs ys
                             | n -> n
-    | (_, _) -> 0
 
 // 3. Make a function
 // compareAuthors : BibliographyItem -> BibliographyItem -> int

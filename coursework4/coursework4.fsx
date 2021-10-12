1eedeab9fa8aee6dcba43e1be0986c25fb015d0e
Tavo Annus <tavo.annus@gmail.com>
2021-10-12 21:56:37 +0300

    ..

--- coursework4/coursework4.fsx
+++ coursework4/coursework4.fsx
@@ -275,7 +275,7 @@ let test = Object [
   ]
 
 let rec listPaths (e : Ecma) : Path list =
-  let prefixAll v xs = match xs with | [] -> [[]] | xs -> List.map (fun ys -> v :: ys) xs  // TODO: Why this broken: List.map ( (::) v) xs
+  let prefixAll v xs = match xs with | [] -> [] | xs -> List.map (fun ys -> v :: ys) xs  // TODO: Why this broken: List.map ( (::) v) xs
   match e with
   | Object o -> [] :: List.collect (fun (name, value) -> prefixAll name (listPaths value)) o
   | List l -> [] :: List.filter (fun v -> match v with [] -> false | _ -> true) (List.collect (listPaths) l)

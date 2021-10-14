bb226ba0d90c8bb006e12826f0251b09d175b351
tavo <taannu@ttu.ee>
2021-10-14 11:05:04 +0300

    khmm

--- coursework4/coursework4.fsx
+++ coursework4/coursework4.fsx
@@ -348,9 +348,9 @@ let rec deletePath (path : Path) (e : Ecma) : Ecma =
                                          // | _ -> n <> p) o)
     | List l -> // List (List.map (deletePath [p]) l)
       List (List.foldBack (fun v acc -> match v with 
-                                        | Object o -> (deletePath [p] v) :: acc // match deletePath [p] v with
-                                                     // | Object [] -> (Object []) :: acc
-                                                      // | xs -> xs :: acc
+                                        | Object o -> match deletePath [p] v with
+                                                      | Object [] -> (Object []) :: acc
+                                                      | xs -> xs :: acc
                                         | v -> v :: acc) l [])
     | e -> e
   | p :: ps ->

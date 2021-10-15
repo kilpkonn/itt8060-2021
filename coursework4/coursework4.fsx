1646991446eb185c6b1268ef911703fa580e8e4e
Tavo Annus <tavo.annus@gmail.com>
2021-10-15 20:19:11 +0300

    ..

--- coursework4/coursework4.fsx
+++ coursework4/coursework4.fsx
@@ -416,5 +416,5 @@ let rec selectPath (ps : Path) (e : Ecma) : Ecma list =
     | _ -> []
 
 let withPath (ps : Path list) (e : Ecma) : Ecma list =
-  List.collect (fun p -> selectPath p e) (List.distinct ps)
+  List.distinct (List.collect (fun p -> selectPath p e) (List.distinct ps))
 

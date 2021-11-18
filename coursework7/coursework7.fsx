df16a71984560ee51439cdcc535ff214642b4965
Tavo Annus <tavo.annus@gmail.com>
2021-11-18 12:16:03 +0200

    .

--- coursework7/coursework7.fsx
+++ coursework7/coursework7.fsx
@@ -103,7 +103,7 @@ let pathWf (p : Path) : bool =
    property? Depends on depth and width but big
 *)
 let createIsWf (p : Path) (t : FsTree) : Property =
-  (pathWf p && fsTreeWf t) ==> (FileSystem.create p t |> fsTreeWf)
+  (pathWf p && fsTreeWf t) ==> fsTreeWf (create p t)
 
 
 

6c537709c80b0d34bcdff13d0f1148deda745116
Tavo Annus <tavo.annus@gmail.com>
2021-11-18 12:27:08 +0200

    o

--- coursework7/coursework7.fsx
+++ coursework7/coursework7.fsx
@@ -75,7 +75,7 @@ open FileSystem
 let rec fsTreeWf (t : FsTree) : bool =
   let subdirs = t.children |> List.map (fun v -> v.name)
   let currOk = List.distinct subdirs = subdirs && t.name <> ""
-  let childOk = List.fold (fun acc ct -> acc && fsTreeWf ct) true t.children
+  let childOk = List.forall fsTreeWf t.children
   currOk && childOk
 
 
@@ -103,7 +103,7 @@ let pathWf (p : Path) : bool =
    property? Depends on depth and width but big
 *)
 let createIsWf (p : Path) (t : FsTree) : Property =
-  pathWf p ==> fsTreeWf (create p t)
+  (pathWf p && fsTreeWf t) ==> fsTreeWf (create p t)
 
 
 

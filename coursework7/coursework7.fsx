6b13886588bf0aaffe77524c8677420a599d5f43
Tavo Annus <tavo.annus@gmail.com>
2021-11-18 22:47:07 +0200

    .

--- coursework7/coursework7.fsx
+++ coursework7/coursework7.fsx
@@ -148,8 +148,8 @@ let wfTrees : Gen<FsTree> =
     | m when m <= 0 -> Gen.constant { name = n; children = [] }
     | _ ->
       gen {
-        let! i = Gen.choose (1, 2 * k)
-        let names = Gen.sample 2 i randStr |> set |> Set.toList
+        let! i = Gen.choose (1, k)
+        let names = Gen.sample (i/10) i randStr |> set |> Set.toList
         let c = names |> List.collect (fun m -> Gen.sample 1 1 (wfTree m (k / 3)))
         return { name = n; children = c}
       }

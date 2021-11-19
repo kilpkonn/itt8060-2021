1b15312e4c5c239d4d1ae145fc5f30e99d53bbd6
Tavo Annus <tavo.annus@gmail.com>
2021-11-19 14:21:07 +0200

    .

--- coursework7/coursework7.fsx
+++ coursework7/coursework7.fsx
@@ -284,4 +284,10 @@ let showShowsEverything (t : FsTree) : bool =
    You may assume that this property is only used with "well-formed"
    generators (meaning that fs, p1 and p2 are well-formed).
 *)
-let createAndDelete (t : FsTree) (p1 : Path) (p2 : Path) : Property = failwith "todo"
+let createAndDelete (t : FsTree) (p1 : Path) (p2 : Path) : Property =
+  let rec startsWith xs ys =
+    match (xs, ys) with
+    | ([], _) -> true
+    | (x :: xs, y :: ys) -> if x = y then startsWith xs ys else false
+    | _ -> false
+  (not (startsWith p1 p2)) ==> (t |> create p1 |> create p2 |> delete p1 |> show |> List.contains p2)
